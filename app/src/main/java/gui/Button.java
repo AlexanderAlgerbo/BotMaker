42b412afcafdce40dd098ea2fce36dfc6bc58a39
Alexander Algerbo <alexander@algsoft.com>
2025-04-03 20:08:21 +0200

    Decided on null layout. Reworked the buttons and events

--- app/src/main/java/gui/Button.java
+++ app/src/main/java/gui/Button.java
@@ -8,36 +8,44 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import javax.swing.JButton;
 import javax.swing.JPopupMenu;
 
+import system.Logic;
+
 public class Button extends JButton {
     private final JPopupMenu menu;
     private AtomicBoolean show;
     // This button class will display a popupmenu when pressed.
     // Will probably make an icon of a arrow pointing down to signify a drop down menu.
-    public Button(JPopupMenu menu){
+    public Button(JPopupMenu menu, int width, int height){
         super();
-        this.setSize(40, 50);
+        //setSize(new Dimension(width, height));
         this.menu = menu;
         show = new AtomicBoolean();
         show.set(false);
-
+        this.menu.setLocation(getX() + width, getY() + height);
+        System.out.println(menu.getX()+ "," + menu.getY());
         // Should make it so that if the popupmenu goes out of focus it stops showing the menu.
         this.addActionListener(e -> {
             show.set(true);
             System.out.println("Popupmenu show");
-            menu.setFocusable(true);
-            menu.setVisible(show.get());
+            this.menu.setFocusable(true);
+            this.menu.setVisible(show.get());
         });
         // As i only need one method, the mouse pressed i will itilize mouse Adapter
         Toolkit.getDefaultToolkit().addAWTEventListener(event -> {
             if (event instanceof MouseEvent mouseEvent) {
                 System.out.println("Menu should close as mouse is pressed");
+                if(!Logic.contains(mouseEvent.getPoint().x, mouseEvent.getPoint().x, this.menu.getX(), this.menu.getY(), this.menu.getWidth(), this.menu.getHeight())){
+                    // Fungerar för x position. INte för Y position. Se till att menyn också hamnar efter buttonen.
+                    this.menu.setVisible(false);
+                }
                 System.out.println("Global Mouse Click at: " + mouseEvent.getPoint());
-                menu.setVisible(false);
+                
+                //mouseEvent.getSource();
+
             }
         }, AWTEvent.MOUSE_EVENT_MASK);
-        // Popup menu is not killed when shutting down the window.
-        // So as i have gathered it i have two options for my listening to mouse clicks. I either have a mouse click on the Jpanel and then have to go through every button, check if
-        // They have their popupmenu displayed. If they do and the mouse is not currently intersecting that component turn of visibility.
-        // Or i add a global mouse listener so that every button listens for all mouse clicks. Then turns off their popup menu if the click is not on the popupmenu
+
     }
+
+
 }
--- app/src/main/java/gui/Gui.java
+++ app/src/main/java/gui/Gui.java
@@ -1,37 +1,34 @@
 package gui;
-// Will contain the UI for the block code. It will be this class one runs i think.
-// So From what i have gathered i will create a ui where one can add instructions to a list. These instructions will have a pulldown bar with different instructions like press A
-// Hold A, or such. Then one can press create script. The instructions will be read and turned into an autohotkey script. Which will be compiled and turned into an exe file.
-// This will allow any person to create a botting program as they please relatively easily without having to type code. They can instead just put in block of instructions.
-// I also want to be able to read scripts and turn them into instructions. So if one needs to edit a script after having closed the program once they can do so.
-// Additionally it might be interesting to create another program which maps/simulates the movements and actions taken by the bot.
 
 
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JPopupMenu;
-import javax.swing.JScrollPane;
 public class Gui{
     private JFrame frame;
     private JPanel panel;
-    private JScrollPane scrollPane = new JScrollPane();
+    JPopupMenu popupMenu = new JPopupMenu();
     // Kanske vill ha alla dessa i en lista senare.
     private List<String> actions = new ArrayList<>();
-    private List<Button> buttons = new ArrayList<>();
+    private List<JButton> buttons = new ArrayList<>();
     public Gui(List<String> actions){
         frame = new JFrame();
         panel = new JPanel();
+        panel.setLayout(null);
         //scrollPane.add(new JButton("Hello"));
         this.actions = actions;
         frame.setTitle("BotMaker");
-        frame.setSize(800,800);
         this.actions = actions;
+        initPopup();
         addPopupMenu();
-        frame.add(scrollPane);
+        addPopupMenu();
+        frame.add(panel);
+        frame.setSize(800,800);
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         frame.setVisible(true);
 
@@ -41,12 +38,7 @@ public class Gui{
     public void addToList(String s){
         actions.add(s);
     }
-
-    // Vi börjar med att skapa en drop down menu.
-    // Man väljer vilken action. Sen kommer ett objekt placeras där istället med text boxar eller annat som man använder för att ställa in
-    // Till exempel om man väljer action Press W. Så kommer en textBox placeras om hur länge.
-    private void addPopupMenu(){
-        JPopupMenu popupMenu = new JPopupMenu();
+    private void initPopup(){
         List<JMenuItem> menuItems = new ArrayList<>();
         for(int i = 0; i < actions.size();i++){
             // För varje action som är implementerad så vill vi displaya den som ett alternativ för blocket. Vill nog definitivt skapa en factory class som skapar
@@ -54,9 +46,22 @@ public class Gui{
             menuItems.add(new JMenuItem(actions.get(i)));
             popupMenu.add(menuItems.get(i));// Lägger till vårat item i popupmenyn.
         }
+    }
+    // Vi börjar med att skapa en drop down menu.
+    // Man väljer vilken action. Sen kommer ett objekt placeras där istället med text boxar eller annat som man använder för att ställa in
+    // Till exempel om man väljer action Press W. Så kommer en textBox placeras om hur länge.
+    private void addPopupMenu(){
+        
         // Here our popup menu should be finished so we can create our button and add it to a list as i want to be able to remove it later when using blocks.
-        buttons.add(new Button(popupMenu));
-        frame.add(buttons.getLast());
+        buttons.add(new JButton());
+        buttons.getLast().setBounds(0, (buttons.size()-1)*150, 100, 150);
+        panel.add(buttons.getLast());
+        panel.revalidate(); // Make sure the layout is updated
+        panel.repaint(); 
         System.out.println("Here");
     }
+
+    // Dags att skapa en metod för att displaya popupmenyn på buttonen som man tryckte
+    // Vi kör sen att vi har en knapp för att spawna alla Block. Vi hade kunnat testa på en drag and drop typ av menu. Eller så gör jag så att block som repeat bara får
+    // Sin egna add knapp. Vi kan testa drag and drop först.
 }
